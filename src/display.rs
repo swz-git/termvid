9e4e505e431761096a8b1eff33ba19786355a426
swz-git <contact@swz.works>
2023-02-22 04:13:42 +0100

    working!

---
+++ src/display.rs
@@ -0,0 +1,97 @@
+use std::{error::Error, fmt::Write};
+
+use console::Term;
+use y4m::Frame;
+use yansi::{Color, Style};
+
+use crate::PixelStyle;
+
+fn get_item_from_brightness<T>(brightness: &u8, display_chars: &[T]) -> T
+where
+    T: Copy,
+{
+    let len = display_chars.len();
+    display_chars[(*brightness as f32 / u8::MAX as f32 * (len - 1) as f32).floor() as usize]
+}
+
+// thank chat gpt for this
+fn yuv_to_rgb(y: f64, u: f64, v: f64) -> (u8, u8, u8) {
+    let y = y * 255.0;
+    let u = u - 128.0;
+    let v = v - 128.0;
+
+    let r = (y + 1.13983 * v).round().max(0.0).min(255.0) as u8;
+    let g = (y - 0.39465 * u - 0.58060 * v).round().max(0.0).min(255.0) as u8;
+    let b = (y + 2.03211 * u).round().max(0.0).min(255.0) as u8;
+
+    (r, g, b)
+}
+
+pub fn display(
+    frame: Frame,
+    display_chars: &[char],
+    colored: bool,
+    pixel_style: PixelStyle,
+) -> Result<(), Box<dyn Error>> {
+    let mut term = Term::stdout();
+    term.clear_screen()?;
+    let mut buf = String::new();
+
+    let term_size = term.size();
+
+    let add_pad = term_size.1 % 2 != 0 && false;
+
+    //
+    let last_color = Color::Unset;
+
+    let y_plane = frame.get_y_plane();
+    let u_plane = frame.get_u_plane();
+    let v_plane = frame.get_v_plane();
+
+    for (i, y_value) in y_plane.iter().enumerate() {
+        let rgb = yuv_to_rgb(
+            *y_value as f64 / u8::MAX as f64,
+            u_plane[i] as f64,
+            v_plane[i] as f64,
+        );
+        // dbg!(rgb, (*y_value, u_plane[i], v_plane[i]));
+        let mut new_color = match colored {
+            false => Color::RGB(*y_value, *y_value, *y_value),
+            true => Color::RGB(rgb.0, rgb.1, rgb.2),
+        };
+        if new_color == last_color {
+            new_color = Color::Unset
+        }
+
+        match pixel_style {
+            PixelStyle::Char => {
+                Style::new(Color::Unset)
+                    .bg(Color::Black)
+                    .fg(new_color)
+                    .fmt_prefix(&mut buf)?;
+            }
+            PixelStyle::Pixel => {
+                Style::new(Color::Unset)
+                    .fg(new_color)
+                    .bg(new_color)
+                    .fmt_prefix(&mut buf)?;
+            }
+        }
+
+        let color = match pixel_style {
+            PixelStyle::Char => get_item_from_brightness(y_value, display_chars),
+            PixelStyle::Pixel => ' ',
+        }
+        .to_string();
+        buf.write_str(&color)?;
+
+        // write empty char at end of line if needed as padding
+        if ((i + 1) % (term_size.1 / 2) as usize) as isize == 0 && add_pad {
+            buf.write_str(" ")?;
+        }
+    }
+
+    std::io::Write::write(&mut term, &buf.bytes().collect::<Vec<u8>>())?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
-use std::path::PathBuf;
+use std::{error::Error, path::PathBuf, process::Stdio};
 
 use clap::Parser;
-mod ffmpeg;
+use console::Term;
+use std::{io::BufReader, process::Command};
+use which::which;
+use yansi::Paint;
 
-/// Simple program to greet a person
+mod display;
+use display::display;
+
+const ASCII_BY_BRIGHTNESS: &str =
+    r#"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. "#;
+
+#[derive(clap::ValueEnum, Clone, Copy, Debug)]
+pub enum PixelStyle {
+    Char,
+    Pixel,
+}
+
+/// CLI application to play a video in the terminal
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -11,16 +26,116 @@ struct Args {
     #[arg(short, long)]
     input: PathBuf,
 
-    /// Framerate to playback at
-    #[arg(short, long, default_value_t = 1)]
-    framerate: u8,
+    /// Enable audio playback
+    #[arg(short, long, default_value_t = false)]
+    audio: bool,
+
+    /// Enable color
+    #[arg(short, long, default_value_t = false)]
+    color: bool,
+
+    /// Pixel style
+    #[arg(value_enum, short, long, default_value_t = PixelStyle::Char)]
+    pixel_style: PixelStyle,
 }
 
-#[tokio::main]
-async fn main() {
+fn exit_sequence() {
+    let term = Term::stdout();
+    let (x, y) = (term.show_cursor(), term.clear_screen());
+    x.unwrap();
+    y.unwrap();
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
-    let test = ffmpeg::split_video(args.input, (1920, 1080), true).await;
+    if args.audio {
+        todo!("audio playback")
+    }
 
-    dbg!(test.expect("shit"));
+    let path = args.input;
+
+    let ffmpeg_path = which("ffmpeg").ok().ok_or(
+        "Couldn't find ffmpeg in path (get it here: https://ffmpeg.org/download.html)".to_owned(),
+    )?;
+
+    ctrlc::set_handler(|| {
+        exit_sequence();
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    let mut command = Command::new(ffmpeg_path);
+
+    let term = Term::stdout();
+
+    let term_size = term.size();
+
+    let ffmpeg_res = (term_size.1, term_size.0);
+
+    let command_args = [
+        "-re",
+        "-i",
+        path.to_str().unwrap(),
+        "-filter_complex",
+        &format!(
+            "scale=iw*2:ih,scale=-1:{},pad=width={}:height={}:x=({}-iw)/2:y=({}-ih)/2:color=black,format=yuv444p",
+            ffmpeg_res.1, ffmpeg_res.0, ffmpeg_res.1,ffmpeg_res.0, ffmpeg_res.1
+        ),
+        "-f",
+        "yuv4mpegpipe",
+        "-loglevel",
+        "quiet",
+        "-",
+    ];
+
+    dbg!("ffmpeg ".to_owned() + &command_args.join(" "));
+
+    command.args(command_args);
+
+    command.stdout(Stdio::piped());
+    command.stderr(Stdio::inherit());
+
+    let mut proc = command.spawn()?;
+
+    let proc_stdout = proc
+        .stdout
+        .take()
+        .ok_or("Failed to read stdout of ffmpeg")?;
+    let reader = BufReader::new(proc_stdout);
+
+    let mut dec = y4m::decode(reader)?;
+
+    // You need to enable ansi stuff on windows smh
+    Paint::enable_windows_ascii();
+
+    term.hide_cursor()?;
+
+    let mut i = 0;
+    loop {
+        let frame = match dec.read_frame() {
+            Ok(a) => a,
+            _ => break,
+        };
+        if i == 0 {
+            i += 1;
+            continue;
+        }
+        display(
+            frame,
+            &ASCII_BY_BRIGHTNESS.chars().collect::<Vec<char>>(),
+            args.color,
+            args.pixel_style,
+        )?;
+
+        i += 1;
+    }
+
+    let proc_result = proc.wait_with_output()?;
+
+    if !proc_result.status.success() {
+        Err(format!("ffmpeg failed"))?
+    };
+
+    exit_sequence();
+    Ok(())
 }
